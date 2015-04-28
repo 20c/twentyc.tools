dc75cde1e59b431f2db01df0667c56d91d102d6f
Stefan Pratter <stefan@20c.com>
2015-04-28 09:58:58 +0000

    prepare for pip release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='twentyc.tools',
-    version='0.1',
+    version='0.1.2',
     author='Twentieth Century',
     author_email='code@20c.com',
     description='various python tool libraries / helpers',
@@ -13,6 +13,8 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    url='https://github.com/20c/twentyc.tools',
+    download_url='https://github.com/20c/twentyc.tools/tarball/0.1.2',
     packages=['twentyc.tools'],
     install_requires=open("requirements.txt").read().split("\n"),
     namespace_packages=['twentyc'],
